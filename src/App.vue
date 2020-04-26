04a2e456d770682eeb927d0327cf87fba2a46593
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-04-26 17:54:51 -0600

    - added project files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <main>
+      <HomePage />
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+}
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
